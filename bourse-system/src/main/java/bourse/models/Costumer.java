c382829b6396e6fe4239589ddfa9f28f7da57ebc
jasminehz <hsh8751@gmail.com>
2017-12-29 12:10:36 +0000

    update Costumer

---
+++ bourse-system/src/main/java/bourse/models/Costumer.java
@@ -0,0 +1,83 @@
+package bourse.models;
+
+import java.util.List;
+
+public class Costumer {
+
+	private String name;
+	private int idClient ;
+	private Brocker brocker;
+	private List<SaleCommand> soldCommands; // commandes vendu
+	private List<BuyCommand> boughtCommands; // commandes achetés
+	private Wallet wallet;
+	private Account account;
+	
+	public Costumer(String name) {
+		this.name=name;
+		this.idClient= idClient++;
+		this.wallet= new Wallet();
+		this.account = new Account(0);
+	}
+	
+	public Costumer(String name, double moneyInTheAccount) {
+		this.name=name;
+		this.idClient= idClient++;
+		this.wallet= new Wallet();
+		this.account = new Account(moneyInTheAccount);
+	}
+	
+	public  String getName() {
+		return name;
+	}
+	public  int getIdClient() {
+		return idClient;
+	}
+	public Brocker getBroker() {
+		return brocker;
+	}
+	public  List<SaleCommand> getSoldCommands() {
+		return soldCommands;
+	}
+	public  List<BuyCommand> getBoughtCommands() {
+		return boughtCommands;
+	}
+	public  Wallet getWallet() {
+		return wallet;
+	}
+	
+	public  void setName(String n) {
+		this.name=n;
+	}
+	public  void setIdClient(int id) {
+		this.idClient=id;
+	}
+	public void setBroker(Brocker b) {
+		this.brocker= b;
+	}
+	public void  setSoldCommands(List<SaleCommand> l) {
+		this.soldCommands =l ;
+	}
+	public void  setBoughtCommands(List<BuyCommand> l) {
+		this.boughtCommands=l;
+	}
+	public void setWallet(Wallet w) {
+		this.wallet=w;
+	}
+	// faire maj du compte des que reception accord du courtie
+	
+public void brockerSubscription() {
+	
+}
+
+public void SRD_Acquittement() {
+	
+}
+
+	public void sendCommand(C) { //  on peut envoyer qu'a un courtier 
+		// dépend protocle . envoi req serveur = brocker
+		
+	}
+	 public static void main (String[] args ) {
+		 
+	}
+}
