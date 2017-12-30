9482f1aa74140ce86297acfab92e60ddb112a458
jasminehz <hsh8751@gmail.com>
2017-12-30 08:51:54 +0000

    modification brocker subscription dans Costumer

--- bourse-system/src/main/java/bourse/models/Bourse.java
+++ bourse-system/src/main/java/bourse/models/Bourse.java
@@ -1,20 +1,5 @@
 package bourse.models;
 
-import java.util.List;
-
-package bourse.models;
-
-import java.io.BufferedReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-package bourse.models;
-
-import java.util.List;
-
-package bourse.models;
-
 import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,61 +9,63 @@ public class Bourse {
 
 	public List<Company> campanies; // List des entreprises
 
-	public List<Brocker> brockerConnecty  = null ; // liste des courtiers ainsi que que
-											// le nombre des clients servis
-	
-	
+	public List<Brocker> brockerConnecty ; // liste des courtiers ainsi que que
+											// le nombre des clients servi
 	
 	public Bourse() {
 		// TODO Auto-generated constructor stub
 	}
-	 
-	 public List<Integer> getResultatBoughtCommand() {
-	        List<Integer> resultat = new ArrayList<Integer>();
-	        
-	        for(Company c : campanies) {
-	            int cpt = 0;
-	            System.out.println("la companie "+c.getName()+ "a l'ordre suivant :"+c.getBoughtCommand());
-	                    cpt++;
-	            resultat.add(cpt);
-	        }
-	        return resultat;
-	    }
-	 public List<Integer> getResultatCommandsold() {
-	        List<Integer> resultat = new ArrayList<Integer>();
-	        
-	        for(Company c : campanies) {
-	            int cpt = 0;
-	            System.out.println("la companie "+c.getName()+ "a l'ordre suivant :"+c.getSoldCommand());
-	                    cpt++;
-	            resultat.add(cpt);
-	        }
-	        return resultat;
-	    }
-	 
-	 public String afficheResultatCommand() {
-	        List<Integer>  resultatbuy= getResultatBoughtCommand() ;
-	        List<Integer>  resultatsold= getResultatCommandsold() ;
-	        String commande = "\n---------"
-	                + "\n  les commandes en attente  sont   "
-	                + "\n----------------------------";
-	        for(int i = 0; i < campanies.size(); i++) {
-	        				if("c'est buy") {
-	        					 commande+="\n "+ (i+1) + " " + campanies.get(i).getName() + " : " +resultatbuy.get(i)  ;
-	        				}
-	        				else {
-	        					
-	        					commande+="\n "+ (i+1) + " " + campanies.get(i).getName() + " : " +resultatsold.get(i)  ;
-	        				}
-	        }
-	            
-	        return commande;
-	    }
-	    
-
 	
-	 
-	 // methode closeBourse () pour fermer la connexion 
-	 
-	 
-}
+	// methode pour recuperer les couriters connecter et les nombres client servant avec 
+	// ilf aut creer une methode getnbclient dans courtier  .
+	public String TotalBrockernbClient(){
+		String message =  " les courtiers connectés et nombre de clients associés sont " ;
+		for(Brocker brocker: brockerConnecty)
+				 message = message + "le Courtier "+brocker.getName()+ " a  "+brocker.getNbClient()+" client(s)";
+		return message;
+	}
+	
+	
+	//methode pour retourner toutes les informations concernant le prix actuel du stock et le nombre d'actions flottantes du stock pour chaque company 
+	public String getFloatActionts(){
+		String message = "" ;
+		for(Company compagny: campanies)
+			message = message + " " +compagny.toString();
+	return message;	
+	
+	}
+	
+	public List<String> getSaleCommandWaiting(){
+	String message = " les commandes de type vendu sont ";
+	List<String> resultat = new ArrayList<String>();
+		for(Company compagny: campanies)
+			message = message + " Société" +compagny.getName()+" : " +compagny.getSoldCommand();
+		     resultat.add(message);
+			return resultat;
+		
+	}
+	
+	public List<String> getBuyCommandWaiting() {
+		String message = " les commandes de type achat sont ";
+		List<String> resultat = new ArrayList<String>();
+		for(Company compagny: campanies)
+			message = message + " Société" +compagny.getName()+" : " +compagny.getBoughtCommand();
+			resultat.add(message);
+			return resultat;		
+	}
+
+	public void updateActionsPrice(){
+		int  deltaprix =0;
+		int nvprix = 0;
+		for(Company c : campanies) {
+			  deltaprix = ((c.getStock().getbuy() - c.getStock().getsale()) / (c.getStock().getActionsNbr());
+			  nvprix = c.getStock().getPrixAction() *(1 + deltaprix);
+			  System.out.println("le prix de tous les actions est le suivant : "+nvprix);
+		}}
+			public void  SRD(Command c) {
+				        System.out.println("\n Reception de la commande du  "+c.getBrocker().getName()+ " concernant le client " +c.getClient().getName()+ " : ");
+				     // aps encore fini 
+			}
+			public void SendPricetoBrocker() {
+				
+			}
